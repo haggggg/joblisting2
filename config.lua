761611ac3319e5234387dcbc5ddfb9a86875f728
haggggg <49823136+haggggg@users.noreply.github.com>
2020-06-17 19:17:22 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+Config.jobs = {
+    {Description = "Become Miner" , Label = "Miner" , Value = "miner" , Emoji = "⛏️" },
+    {Description = "Become Tailor" , Label = "Tailor" , Value = "tailor" , Emoji = "👔"},
+    {Description = "Become Lumberjack" , Label = "Lumberjack" , Value = "lumberjack" , Emoji = "🪓"},  
+    {Description = "Become Garbage collector" , Label = "Garbage collector" , Value = "garbage" , Emoji = "🗑️"},
+    {Description = "Become Slaughterer" , Label = "Slaughterer" , Value = "slaughterer" , Emoji = "🔪"},
+    {Description = "Become Fisherman" , Label = "Fisherman" , Value = "fisherman" , Emoji = "🎣"}    
+
+}
+
+Config.pos = {
+    {X = -269.32, Y = -956.15, Z = 30.22 }
+}    
